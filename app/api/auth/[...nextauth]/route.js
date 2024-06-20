290461aae1a74f72fbcd3f52a0dfbff6cb4bef2e
marve123 <eric.marvelboy@gmail.com>
2024-06-20 17:45:52 +0100

    a mere trial

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,40 +10,39 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    ],
-    callbacks: {
-        async session({ session }) {
-            const sessionUser = await User.findOne({
-                email: session.user.email
-            })
-    
-            session.user.id = sessionUser._id.toString()
-    
-            return session
-      },
-      async signIn({ profile }) {
-        try {
-          await connectToDB();
-    
-          // check if a user exists
-          const userExists = await User.findOne({ email: profile.email });
-    
-          // if not create user and save to db
-          if (!userExists) {
-            await User.create({
-              email: profile.email,
-              username: profile.name.replace(" ", "").toLowerCase(),
-              image: profile.picture,
-            });
-          }
-    
-          return true;
-        } catch (error) {
-          console.log(error);
-          return false;
-        }
-      },
+  ],
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      });
+      session.user.id = sessionUser._id.toString();
+
+      return session;
     },
+    async signIn({ account, profile, user, credentials }) {
+      try {
+        await connectToDB();
+
+        // check if a user exists
+        const userExists = await User.findOne({ email: profile.email });
+
+        // if not create user and save to db
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log("Error checking if user exists: ", error.message);
+        return false;
+      }
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -6,83 +6,73 @@ import PromptCard from "./PromptCard";
 
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
-  <div className="mt-16 prompt_layout">
-    {data.map((post) => (
-      <PromptCard
-        key={post._id}
-        post={post}
-        handleTagClick={handleTagClick}
-      />
-    ))}
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
 const Feed = () => {
-  const [posts, setPosts] = useState([]);
-
-
+  const [allPosts, setAllPosts] = useState([]);
 
   // Search states
   const [searchText, setSearchText] = useState("");
   const [searchTimeout, setSearchTimeout] = useState(null);
   const [searchedResults, setSearchedResults] = useState([]);
 
-   const filterPrompts = (searchtext) => {
-     const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
-     return posts.filter(
-       (item) =>
-         regex.test(item.creator.username) ||
-         regex.test(item.tag) ||
-         regex.test(item.quote)
-     );
-   };
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
 
-   const handleSearchChange = (e) => {
-     clearTimeout(searchTimeout);
-     setSearchText(e.target.value);
-
-     // debounce method
-     setSearchTimeout(
-       setTimeout(() => {
-         const searchResult = filterPrompts(e.target.value);
-         setSearchedResults(searchResult);
-       }, 500)
-     );
+    setAllPosts(data);
   };
-  
-   const handleTagClick = (tagName) => {
-     setSearchText(tagName);
 
-     const searchResult = filterPrompts(tagName);
-     setSearchedResults(searchResult);
-   };
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
-   const fetchPosts = async () => {
-     try {
-       const response = await fetch("/api/prompt")
-       const data = await response.json()
-       // console.log("Fetched data:", data)
-       setPosts(data);
-     } catch (error) {
-       console.error("Fetch error:", error)
-     }
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+    return allPosts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
   };
-  
-   useEffect(() => {
-     fetchPosts();
-   }, []);
 
-   useEffect(() => {
-     console.log("Posts updated:", posts); // Debugging log ooo, God abeg😫
-   }, [posts]);
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
 
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
         <input
           type="text"
-          placeholder="Search for a tag or username"
+          placeholder="Search for a tag or a username"
           value={searchText}
           onChange={handleSearchChange}
           required
@@ -90,13 +80,14 @@ const Feed = () => {
         />
       </form>
 
+      {/* All Prompts */}
       {searchText ? (
         <PromptCardList
           data={searchedResults}
           handleTagClick={handleTagClick}
         />
       ) : (
-        <PromptCardList data={posts} handleTagClick={handleTagClick} />
+        <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
       )}
     </section>
   );
